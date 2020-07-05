52fd6dbf0317ebf03b0eaacb63477af071a8464f
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-05 03:54:21 +0200

    v0.1.5: haxxterm

---
+++ haxxterm.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+
+function haxxterm () {
+  local RUNMODE="$1"; shift
+
+  local SELFFILE="$(readlink -m -- "$BASH_SOURCE")"
+  local SELFPATH="$(dirname -- "$SELFFILE")"
+  local APPNAME="$FUNCNAME"
+  local SCREENS_LIST="$HOME/.config/Terminal/$APPNAME.screens.txt"
+  local HAS_COLORDIFF=
+  </dev/null colordiff &>/dev/null && HAS_COLORDIFF='colordiff'
+
+  "${FUNCNAME}_${RUNMODE:-spawn}" "$@"
+  return $?
+}
+
+
+function haxxterm_spawn () {
+  local TERM_OPT=(
+
+    # terminal launcher options:
+    --{sessname,winname}="$APPNAME"
+    # -T--hold
+    -- # end of terminal launcher options
+
+    # autoscreen options:
+    -- # end of autoscreen options
+
+    # screen command:
+    "$SELFFILE" welcome
+    )
+  gautoscreen "${TERM_OPT[@]}"
+}
+
+
+function haxxterm_scrl () {
+  ( [ -n "$DISPLAY" ] && leafpad "$SCREENS_LIST"
+    ) || nano "$SCREENS_LIST" || return $?
+  local SCRL_ABS="$(readlink -m -- "$SCREENS_LIST")"
+  sed -re 's~\s+$~~' -i -- "$SCRL_ABS" || return $?
+  # ^-- rel b/c otherwise sed would replace a potential symlink with
+  #     a regular text file.
+}
+
+
+function haxxterm_diff () {
+  if [ -n "$HAS_COLORDIFF" ] && tty --silent <&1; then
+    "$FUNCNAME" | "$HAS_COLORDIFF" | less -rS
+    return 0
+  fi
+  local PATHS_LIST="$(screen-windowlist "$APPNAME" | sed -re '
+    s~^[0-9]+\t[^A-Za-z0-9]*\t~~
+    s~^[a-z0-9_-]+@[a-z0-9_-]+ bash (\~?/)~\1~
+    ')"
+  diff -sU 9009009 -- "$SCREENS_LIST" <(echo "$PATHS_LIST")
+}
+
+
+function haxxterm_welcome () {
+  clear
+  export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
+  local TERM_PATHS=()
+  readarray -t TERM_PATHS < <(sed -nre '
+    s~^\s+~~
+    \:^\~?/:!b
+    s!\s+¶\s+!\n!g
+    p
+    ' -- "$SCREENS_LIST")
+
+  local SC0_DIR="${TERM_PATHS[0]}"
+  case "$SC0_DIR" in
+    '~/'* ) SC0_DIR="$HOME/${SC0_DIR:1}";;
+  esac
+
+  "${FUNCNAME}_prepare" || echo "W: ${FUNCNAME}_prepare rv=$?" >&2
+
+  cd -- "$SC0_DIR" || cd -- "$HOME" || cd -- / || return $?
+  exec bash
+  return $?$(echo "E: exec bash failed: rv=$?" >&2; sleep 5s)
+}
+
+
+function haxxterm_welcome_prepare () {
+  [ "$WINDOW" == 0 ] || return 4$(echo "E: panic: expected WINDOW=0" >&2)
+  cd / || return $?
+
+  xargs screen -X eval < <(sed -nre 's~^\s*([a-z])~\1~p' <<<'
+    defcaption always
+    defvbell on
+    split
+    fit
+    focus next
+    fit
+    windowlist -b
+    focus prev
+    ')
+
+  ( sleep 1s; cdscreen "${TERM_PATHS[@]:1}" ) &
+  return 0
+}
+
+
+
+
+
+
+
+
+
+
+haxxterm "$@"; exit $?
