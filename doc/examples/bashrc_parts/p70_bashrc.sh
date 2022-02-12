2d87b66a9b7787208603d3cb93f4fdcdadf9365f
mk-pmb <mk-pmb@users.noreply.github.com>
2022-02-12 19:39:51 +0100

    Simplify bashrc condition

--- doc/examples/bashrc_parts/p70_bashrc.sh
+++ doc/examples/bashrc_parts/p70_bashrc.sh
@@ -1,3 +1,2 @@
 # -*- coding: utf-8, tab-width: 2 -*-
-[ ! -n "$BASH_VERSION" ] || [ ! -f "$HOME/.bashrc" ] \
-  || source -- "$HOME/.bashrc"
+[ -z "$BASH_VERSION" ] || [ ! -f "$HOME/.bashrc" ] || source -- "$HOME/.bashrc"
