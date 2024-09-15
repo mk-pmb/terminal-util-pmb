bd245a7e7f5e00031a4964d27cad48461b913223
mk-pmb <mk-pmb@users.noreply.github.com>
2024-09-15 23:25:09 +0200

    repeatedly --date-cr

--- repeatedly.sh
+++ repeatedly.sh
@@ -4,17 +4,19 @@
 function repeatedly () {
   local INTV='10s'
   local PREPARE=
+  local DATE_CR=
   local PRESLEEP=
   while [ "$#" -ge 1 ]; do case "$1" in
     --clear ) PREPARE+='clear; '; shift;;
     --pad ) PRESLEEP+='echo; echo; echo; '; shift;;
+    -r | --date-cr ) DATE_CR='\r'; shift;;
     [0-9]* ) INTV="$1"; shift;;
     -- ) shift; break;;
     * ) break;;
   esac; done
   while true; do
     eval "$PREPARE"
-    printf '%(%a %d %T)T ' -1
+    printf "$DATE_CR"'%(%a %d %T)T ' -1
     "$@"
     eval "$PRESLEEP"
     sleep "$INTV" || return $?
