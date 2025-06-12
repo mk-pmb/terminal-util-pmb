6fa3379ee3030547d93209cf7c5bb18509036605
mk-pmb <mk-pmb@users.noreply.github.com>
2025-06-12 20:51:35 +0200

    [docs] Example bashrc_parts: Auto-Installer: Support Uberspace.

--- docs/examples/bashrc_parts/install_default_configs.sh
+++ docs/examples/bashrc_parts/install_default_configs.sh
@@ -15,6 +15,14 @@ function install_main () {
   [ "$REPO_DIR" -ef "$HTUP" ] || return 3$(
     echo "E: Expected '$REPO_DIR' and '$HTUP' to be the same directory." >&2)
 
+  local SAFE_MV='mv --verbose --no-clobber --no-target-directory --'
+  local BACKUP_SUF="orig-$(printf '%(%y%m%d-%H%M)T' -1)-$$"
+
+  local IMPL_PRE= IMPL_SUF=
+  case "$HOSTNAME" in
+    *.uberspace.de ) install_snowflake_uberspace; return $?;;
+  esac
+
   install_rc_file .profile p || return $?
   install_rc_file .bashrc  r || return $?
   install_default_rcd || return $?
@@ -24,10 +32,13 @@ function install_main () {
 function install_rc_file () {
   local DEST="$HOME/$1"
   local IMPL='eval "$("$HOME"'"$TUP_HSUB/$PLUGBS_MAIN $2"')"'
+  [ -z "$IMPL_PRE" ] || IMPL="$IMPL_PRE"$'\n'"$IMPL"
+  [ -z "$IMPL_SUF" ] || IMPL="$IMPL"$'\n'"$IMPL_SUF"
   [ -f "$DEST" ] || echo "$IMPL" >"$DEST" || return $?
   local ACTUAL="$(head --lines=2; echo .)"
   diff -sU 4 --label "expected/$1" -- <(echo "$IMPL") "$DEST" || return $?$(
-    echo "E: Unexpected text in $DEST. Please move that file away." >&2)
+    echo "E: Unexpected text in $DEST. Please move that file away, e.g." \
+      $SAFE_MV "$DEST"'{,.'"$BACKUP_SUF"'}' >&2)
 }
 
 
@@ -50,6 +61,15 @@ function install_default_rcd () {
     $LNS tu.core.rcd/$ITEM $HCB/tu.$ITEM.rcd || return $?$(
       echo "E: Failed to create symlink: $HCB/tu.$ITEM.rcd" >&2)
   done
+}
+
+
+function install_snowflake_uberspace () {
+  local BACKUP_SUF="uberspace-$BACKUP_SUF"
+  install_rc_file .bash_profile p || return $?
+  IMPL_PRE='source -- /etc/bashrc' install_rc_file .bashrc  r || return $?
+  install_default_rcd || return $?
+  $SAFE_MV "$HOME"/.profile{,."$BACKUP_SUF"}
 }
 
 
