6a075919071555cd0961055a9873e13b2f965a32
mk-pmb <mk-pmb@users.noreply.github.com>
2025-06-12 19:10:04 +0200

    [docs] Example bashrc_parts: Support ~/.local/bin in PATH.

--- docs/examples/bashrc_parts/p10_env_homebin.sh
+++ docs/examples/bashrc_parts/p10_env_homebin.sh
@@ -1,3 +1,15 @@
 # -*- coding: utf-8, tab-width: 2 -*-
-[ ! -d "$HOME/bin" ] || [ "${PATH%%:*}" == "$HOME/bin" ] \
-  || PATH="$HOME/bin:$PATH"
+local SUB='
+  .local/bin
+  bin
+  '
+PATH=":${PATH//:/::}:"
+for SUB in $SUB; do
+  SUB="$HOME/$SUB"
+  [ -d "$SUB" ] || continue
+  PATH=":$SUB:${PATH//:$SUB:/}"
+done
+PATH="${PATH//::/:}"
+PATH="${PATH#:}"
+PATH="${PATH%:}"
+export PATH
