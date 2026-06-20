5e4a137e385e3676d1f577d63b66fbe925786b8c
mk-pmb <mk-pmb@users.noreply.github.com>
2026-06-20 06:28:07 +0200

    screen-list-window-processes: List window number and args.

--- screen-list-window-processes.sh
+++ screen-list-window-processes.sh
@@ -9,43 +9,89 @@ function scrl_winprocs () {
     echo "E: $0 $FUNCNAME: This is still so highly experimental that" \
       "you must specify the latest SCRLWP_CLI_VER." >&2)
 
+  local SED_HEXESC='s~\\x%s~\\\\x%s~g;'
+  SED_HEXESC="$(printf -- "$SED_HEXESC" \
+    {5C,0{7..9},0{A..D},1B,20,22,27,7F}{,})"
+  SED_HEXESC="${SED_HEXESC/x5C/'\'}" # sed quirk: doesn't accept \x5C
+
+  scrl_"$@" || return $?
+}
+
+
+function scrl_allsess_scan_winprocs () {
   local SESSIONS=()
-  readarray -t SESSIONS < <(screen -list | sed -nre '
-    s~^\s+([0-9]+)\.(\S+)\s.*$~\1=\2~p')
+  readarray -t SESSIONS < <(screen -list |
+    sed -nre 's~^\s+([0-9]+)\.(\S+)\s.*$~\1=\2~p')
   local SESS_NAME= SESS_PID=
   for SESS_NAME in "${SESSIONS[@]}"; do
     SESS_PID="${SESS_NAME%%=*}"
     SESS_NAME="${SESS_NAME#*=}"
-    echo "* session $SESS_PID: $SESS_NAME"
-    scrl_winprocs_found_sess "$SESS_PID" || return $?
+    scrl_winprocs_found_sess || return $?
   done
 }
 
 
 function scrl_winprocs_found_sess () {
-  local S_PID="$1"
-  local W_PID=
-  for W_PID in $(ps ho pid --ppid "$S_PID"); do
-    echo "  * window $(scrl_winprocs_describe_proc $W_PID)"
+  local WIN_PIDS=( $(ps ho pid --ppid "$SESS_PID" | sort -g) )
+  local N_WINS="${#WIN_PIDS[@]}" WIN_PID=
+  WIN_PID="${WIN_PIDS[*]}"
+  WIN_PID="${WIN_PID// /,}"
+  printf -- '%s\t' = sess="$SESS_NAME" spid="$SESS_PID" \
+    n_wins="$N_WINS" w_pids="$WIN_PID"
+  echo =
+  for WIN_PID in "${WIN_PIDS[@]}"; do
+    scrl_winprocs_describe_proc
   done
 }
 
 
 function scrl_winprocs_describe_proc () {
-  local P_PID="$1"
-  echo -n "pid=$P_PID"
-  </proc/"$P_PID"/environ tr -s '\0' '\n' | sed -nre '
-    s~^(WINDOW)=~ &~p
-    ' | tr -d '\n'
+  printf -- '%s\t' = sess="$SESS_NAME" spid="$SESS_PID"
+  local WIN_ENV='
+    s!^(WINDOW)=!wnum=!p
+    '
+  WIN_ENV="$(sed -znre "$WIN_ENV" -- /proc/"$WIN_PID"/environ |
+    tr -s '\0' '\n' | LANG=C sort -V | tr '\n' '\t')"
+  echo -n "$WIN_ENV"
 
-  local P_CWD="$(LANG=C stat -c '%N' -- /proc/"$P_PID"/cwd)"
-  P_CWD="${P_CWD#* -\> \'}"
-  P_CWD="${P_CWD%\'}"
-  echo -n " cwd='$P_CWD'"
+  printf -- '%s\t' wpid="$WIN_PID"
 
-  local P_CMD="$(cat -- /proc/"$P_PID"/cmdline)"
-  P_CMD="‹${P_CMD//$'\x00'/› ‹}›"
-  echo " : $P_CMD"
+  #==BEGIN== Read proc symlinks ===== ===== ===== ===== ===== ===== =====
+  local -A SYMLINKS=()
+  local KEY= VAL=
+  for KEY in exe cwd ; do
+    VAL="$(LANG=C stat -c '%N' -- "/proc/$WIN_PID/$KEY")"
+    VAL="${VAL#* -\> \'}"
+    VAL="${VAL%\'}"
+    SYMLINKS["$KEY"]="$VAL"
+    printf -- '%s\t' "$KEY=$VAL"
+  done
+  #==ENDOF== Read proc symlinks ===== ===== ===== ===== ===== ===== =====
+
+  local P_ARGS="$(LANG=C sed -zre "$SED_HEXESC" \
+    -- /proc/"$WIN_PID"/cmdline | tr '\0' '\t')"
+  P_ARGS="${P_ARGS%$'\t'}"
+  local P_ARG0="${P_ARGS%%$'\t'*}"
+  P_ARGS="${P_ARGS:${#P_ARG0}}"
+
+  local P_ALIAS="$P_ARG0"
+  case "$P_ALIAS" in
+    "${SYMLINKS[exe]}" ) P_ALIAS=;;
+  esac
+  printf -- '%s\t' alias="$P_ALIAS"
+
+  local N_ARGS="${P_ARGS//[^$'\t']/}"
+  N_ARGS="${#N_ARGS}"
+  printf -- '%s\t' n_args="$N_ARGS"
+
+  echo -n =
+  if [ -n "$P_ARGS" ]; then
+    P_ARGS='"'"${P_ARGS#$'\t'}"'"'
+    P_ARGS="${P_ARGS//$'\t'/$'"\t"'}"
+    echo -n $'\t'"$P_ARGS"
+  fi
+
+  echo
 }
 
 
